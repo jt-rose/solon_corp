ced4ae082cb03ed2496915eefde514ce37b28e2e
Jeffrey Rose <jtr219@outlook.com>
2022-02-02 16:26:22 -0500

    set up basic routing

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ app.get("/", (_req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/contact", (_req, res) => {
+  res.render("contact.ejs");
+});
+
+app.get("/history", (_req, res) => {
+  res.render("history.ejs");
+});
+
+app.get("/links", (_req, res) => {
+  res.render("links.ejs");
+});
+
+app.get("/past-work", (_req, res) => {
+  res.render("past-work.ejs");
+});
+
+app.get("/staff", (_req, res) => {
+  res.render("staff.ejs");
+});
+
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
